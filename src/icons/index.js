41e352ed6000d4168e1d36d9975be2e7fd65fafe
summer <intoyingyingmiao@163.com>
2022-09-23 17:13:45 +0800

    feate项目初始化完成

--- src/icons/index.js
+++ src/icons/index.js
@@ -5,5 +5,14 @@ import SvgIcon from '@/components/SvgIcon'// svg component
 Vue.component('svg-icon', SvgIcon)
 
 const req = require.context('./svg', false, /\.svg$/)
+// req函数===>根据路径 对应的模块 导入进来
+
+// 函数keys属性===>方法 ====>数组
+// console.log(req.keys())
+console.log(require.context('./svg', false, /\.svg$/)('./dashboard.svg'))
+// console.log(req('./dashboard.svg'))
+// console.log(req => req.keys().map(req))
+// console.log(req.keys().map(req))
 const requireAll = requireContext => requireContext.keys().map(requireContext)
 requireAll(req)
+// console.log(requireAll(req))
